033d1b040546131cfdc87fc119cb9c5d2942a2d0
Ethan <ethanwright98@hotmail.co.uk>
2022-05-22 21:51:32 +0000

    adding api4 & updating app

--- api1/venv/bin/activate.fish
+++ api1/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ethan/project/api1/venv"
+set -gx VIRTUAL_ENV "/home/ethan/Project3/api1/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
